3e14d5c5177c2d400b335154774648dc7ad8fed1
Aleksandr Kostiuk <kosskill2011@gmail.com>
2018-09-21 10:17:45 +0200

    new

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var liczbaPierwsza = 10;
 var liczbaDruga = 15;
 function suma(liczbaPierwsza, liczbaDruga) {
- return liczbaPierwsza + liczbaDruga;
+ return [liczbaPierwsza + liczbaDruga];
 }
+console.log(suma);
 
